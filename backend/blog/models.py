f15a48e9caf70069a21af4a183ea628c0dc0cbd8
SamaDaw <samadaw3@gmail.com>
2025-09-18 22:30:22 +0300

    register the blog in the admin

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class 
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    article = models.TextField()
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
